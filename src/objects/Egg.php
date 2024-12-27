455d09e146d7fec43075ad503e9f081b769aa570
Sean Hurley <sean.hurley170@gmail.com>
2024-12-26 21:58:02 -0800

    made the egg class use the new Monster class

--- src/objects/Egg.php
+++ src/objects/Egg.php
@@ -2,10 +2,12 @@
 
 namespace PokePHP\Src\Objects;
 
+use PokePHP\Src\Objects\Abstract\Monster;
+
 class Egg {
     // matching gen 9
     public const STEPS_PER_CYCLE = 128;
-    public int $monsterId;
+    public Monster $species;
     public int $stepsTaken;
     public int $cyclesRequired;
 }
